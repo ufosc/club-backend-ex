05cabea16970a501dca5007fb0bc7169eb1d0f7e
Hunter Jarrell <hjarrell555@gmail.com>
2020-07-25 18:05:49 -0400

    Add user model

---
+++ lib/club_backend/accounts/user.ex
@@ -0,0 +1,21 @@
+defmodule ClubBackend.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "users" do
+    field :email, :string
+    field :password_hash, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :password_hash, :email])
+    |> validate_required([:username, :password_hash])
+  end
+end
---
+++ priv/repo/migrations/20200725220419_create_users.exs
@@ -0,0 +1,17 @@
+defmodule ClubBackend.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :username, :string
+      add :password_hash, :string
+      add :email, :string
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:username])
+    create unique_index(:users, [:email])
+  end
+end
